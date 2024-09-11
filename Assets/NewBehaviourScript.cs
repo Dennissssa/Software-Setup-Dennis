08364d5eda5f195010dc6c23f7b6c7114ad3f9db
Dennissssa <160363087+Dennissssa@users.noreply.github.com>
2024-09-10 23:07:41 -0700

    The project

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float Dennisvariable = 1;
+        Dennisvariable = 2;
+        Dennisvariable = Dennisvariable + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
